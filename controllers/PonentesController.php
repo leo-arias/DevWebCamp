6554fadaeab9135c6b91c72fbc5c33e67b971d63
Leo <143009663+leo-arias@users.noreply.github.com>
2024-01-27 01:38:40 -0300

    Fix redirection issues and add pagination to registered users page

--- controllers/PonentesController.php
+++ controllers/PonentesController.php
@@ -9,6 +9,10 @@ use Intervention\Image\ImageManagerStatic as Image;
 
 class PonentesController {
     public static function index(Router $router) {
+        if(!is_admin()) {
+            header('Location: /login');
+        }
+
         $pagina_actual = $_GET['page'];
         $pagina_actual = filter_var($pagina_actual, FILTER_VALIDATE_INT);
 
@@ -26,10 +30,6 @@ class PonentesController {
         }
 
         $pomentes = Ponente::paginar($registros_por_pagina, $paginacion->offset());
-
-        if(!is_admin()) {
-            header('Location: /login');
-        }
 
         $router->render('admin/ponentes/index', [
             'titulo' => 'Ponentes / Conferencistas',
